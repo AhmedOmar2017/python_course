cd6d250a101eb0d9832e6a4b29a6145ead5931da
Ahmed Omar <eng.ahmed.omar2017@gmail.com>
2024-06-30 19:14:58 +0300

    concatenation

---
+++ 8-concatenation.py
@@ -0,0 +1,26 @@
+# concatenation => link two strings together
+# we use + sign 
+# you can not concatenat string with number
+
+
+
+msg = "I Love "
+lang = "Python"
+full = msg + " " + lang
+
+print(full)
+
+
+a = "First\
+ sacond\
+third"
+
+
+b = "A\
+B\
+C"
+
+
+print(a+" "+b)
+print(a+"\n"+b)
+
